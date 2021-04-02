6a9333ea5db79d5b037970e83a67986b526e4e84
Fuad Mohd. Firoz <fuad.f@simformsolutions.com>
2021-04-02 16:48:14 +0530

    move provider to App.js

--- app/navigation/AppNavigation.js
+++ app/navigation/AppNavigation.js
@@ -1,26 +1,22 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import * as React from 'react';
-import {Provider} from 'react-redux';
 import AddEditTodoScreen from '../modules/ToDos/AddEditToDo';
 import TodoListScreen from '../modules/ToDos/TodoList';
-import {store} from '../redux/store';
 
 const Stack = createStackNavigator();
 
 const AppNavigation = () => (
-  <Provider store={store}>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={'TodoListScreen'}>
-        <Stack.Screen name="TodoListScreen" component={TodoListScreen} />
-        <Stack.Screen
-          name="AddEditTodoScreen"
-          component={AddEditTodoScreen}
-          initialParams={{editMode: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  </Provider>
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName={'TodoListScreen'}>
+      <Stack.Screen name="TodoListScreen" component={TodoListScreen} />
+      <Stack.Screen
+        name="AddEditTodoScreen"
+        component={AddEditTodoScreen}
+        initialParams={{editMode: false}}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
 );
 
 export default AppNavigation;
